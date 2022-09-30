9ce2c1044f064e0b638e4f971ba0bc2d2395a3fa
n1ierd <n1ierd@SZA0025779>
2022-09-30 11:13:43 +0200

    Hozzáadás funkció

--- gyakorlat_3/gyakorlat_3/Form1.cs
+++ gyakorlat_3/gyakorlat_3/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyakorlat_3.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,17 +13,34 @@ namespace gyakorlat_3
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource.LastName;
             label2.Text = Resource.FirstName;
             button1.Text = Resource.Add;
+
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = label1.Text,
+                FirstName = label2.Text
+            };
+            users.Add(u);
         }
     }
 }
