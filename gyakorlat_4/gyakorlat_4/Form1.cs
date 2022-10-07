0ab4b272dc315246336e617a269e72fa7bcc298e
n1ierd <n1ierd@SZA0025779>
2022-10-07 11:25:59 +0200

    .

--- gyakorlat_4/gyakorlat_4/Form1.cs
+++ gyakorlat_4/gyakorlat_4/Form1.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace gyakorlat_4
 {
     public partial class Form1 : Form
     {
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
+
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            //CreateExcel();
         }
+
+        private void LoadData() 
+        {
+            Flats = context.Flat.ToList();
+        }
+
+        static void CreateExcel()
+        {
+            string[] headers = new string[] {
+                 "Kód",
+                 "Eladó",
+                 "Oldal",
+                 "Kerület",
+                 "Lift",
+                 "Szobák száma",
+                 "Alapterület (m2)",
+                 "Ár (mFt)",
+                 "Négyzetméter ár (Ft/m2)"};
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+        }
+
     }
 }
