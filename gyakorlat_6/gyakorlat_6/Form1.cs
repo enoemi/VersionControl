a327701ecf249c27af0f56822ab525321c4c02ed
n1ierd <n1ierd@SZA0025779>
2022-10-21 11:10:11 +0200

    commit all

--- gyakorlat_6/gyakorlat_6/Form1.cs
+++ gyakorlat_6/gyakorlat_6/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyakorlat_6.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,24 @@ namespace gyakorlat_6
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRatesRequestBody();
         }
+
+        
+
+        var mnbService = new MNBArfolyamServiceSoapClient();
+
+        var request = new GetExchangeRatesRequestBody()
+        {
+            currencyNames = "EUR",
+            startDate = "2020-01-01",
+            endDate = "2020-06-30"
+        };
+
+        var response = MnbService.GetExchangeRates(request);
+
+        var result = response.GetExchangeRatesResult;
+
+        
     }
 }
