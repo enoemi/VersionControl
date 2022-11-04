566421614f9aa425956af6eae3c375d3c8292279
n1ierd <n1ierd@SZA0025779>
2022-11-04 10:45:46 +0100

    initial commit

--- gyakorlat_7/gyakorlat_7/Form1.cs
+++ gyakorlat_7/gyakorlat_7/Form1.cs
@@ -16,5 +16,32 @@ namespace gyakorlat_7
         {
             InitializeComponent();
         }
+
+        public class Ball : Label
+        {
+            public Ball()
+            {
+                AutoSize = false;
+                Width = 50;
+                Height = Width;
+                Paint += Ball_Paint;
+            }
+
+            private void Ball_Paint(object sender, PaintEventArgs e)
+            {
+                DrawImage(e.Graphics);
+            }
+
+            protected void DrawImage(Graphics g)
+            {
+                g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+            }
+
+            public void MoveBall()
+            {
+                Left += 1;
+            }
+        }
+
     }
 }
